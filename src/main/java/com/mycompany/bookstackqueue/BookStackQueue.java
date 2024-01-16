1a1f120eb8eab4725888a954df0f513046121b6a
Erika Bautista <ekabells19@gmail.com>
2024-01-16 21:21:05 +0800

    Update BookStackQueue.java

--- src/main/java/com/mycompany/bookstackqueue/BookStackQueue.java
+++ src/main/java/com/mycompany/bookstackqueue/BookStackQueue.java
@@ -8,9 +8,33 @@ package com.mycompany.bookstackqueue;
  *
  * @author Erika
  */
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 public class BookStackQueue {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        Stack<String> stack = new Stack<>();
+        Queue<String> queue = new LinkedList<>();
+
+        System.out.println("Enter four book titles.");
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Book " + i + ": ");
+            String bookTitle = scanner.nextLine();
+            stack.push(bookTitle);
+        }
+
+        // Pop elements from stack and add to queue
+        while (!stack.isEmpty()) {
+            String bookTitle = stack.pop();
+            queue.add(bookTitle);
+        }
+
+        // Print the content
+        System.out.println("New order of books: " + queue);
     }
 }
